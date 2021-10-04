905d6529c33dabeec4a8d1f17112fab444067bc9
Kaido Järvemets <me@kaidojarvemets.ee>
2021-10-04 20:54:50 +0300

    Update New-AzureArcADServicePrinciple.ps1

--- New-AzureArcADServicePrinciple.ps1
+++ New-AzureArcADServicePrinciple.ps1
@@ -7,12 +7,18 @@ Connect-AzAccount
 Set-AzContext -Subscription XXXXX-XXXXX-XXXX-XXXX-XXXXXXX
 
 #Create new Service Princile and assign "Azure Connected Machine Onboarding" permissions
-$AARCServicePrincipal = New-AzADServicePrincipal -DisplayName "Azure-Arc-for-servers" -Role "Azure Connected Machine Onboarding"
+$NewAADSPNDetails = @{
+    DisplayName = "Azure-Arc-for-servers"
+    Role = "Azure Connected Machine Onboarding"
+}
+
+$AARCServicePrincipal = New-AzADServicePrincipal @NewAADSPNDetails
 
 #Print out the Service Princile information
 $AARCServicePrincipal
 
-#The following code will allow you to export the secre -https://docs.microsoft.com/en-us/powershell/azure/create-azure-service-principal-azureps?view=azps-6.4.0
+#The following code will allow you to export the secre
+#https://docs.microsoft.com/en-us/powershell/azure/create-azure-service-principal-azureps?view=azps-6.4.0
 $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($AARCServicePrincipal.Secret)
 $UnsecureSecret = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
 
