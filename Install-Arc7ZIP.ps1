93744f35d2477639074bc0e6d02e4fdd7a546ea7
Kaido Järvemets <me@kaidojarvemets.ee>
2023-08-26 15:48:26 +0300

    Update Install-Arc7ZIP.ps1

--- Install-Arc7ZIP.ps1
+++ Install-Arc7ZIP.ps1
@@ -1,5 +1,5 @@
-$7ZIPURL = 'https://www.7-zip.org/a/7z2201-x64.exe'
-$SourceFolder = 'C:\Windows\Temp\7z2201-x64.exe'
+$7ZIPURL = 'https://www.7-zip.org/a/7z2301-x64.exe'
+$SourceFolder = 'C:\Windows\Temp\7z2301-x64.exe'
 Invoke-WebRequest -Uri $7ZIPURL -OutFile $SourceFolder
 
-Start-Process -FilePath 'C:\Windows\Temp\7z2201-x64.exe' -ArgumentList "/S"
+Start-Process -FilePath 'C:\Windows\Temp\7z2301-x64.exe' -ArgumentList "/S"
